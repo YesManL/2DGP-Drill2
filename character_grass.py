95b5c764ac1cdca8ef22c50c4e5eb42aadf72302
folder <ht51566@gmail.com>
2025-09-16 21:20:44 +0900

    Drill #2 제출 - 2023182044 임호권
    Drill #2 제출 - 2023182044 임호권

--- character_grass.py
+++ character_grass.py
@@ -6,44 +6,52 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 400
-y = 90
+center_x = 400
+center_y = 300
+radius = 200
+angle = 0
 
-while (x < 780):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
+while True:
+    x, y = 400, 90
+    while x < 780:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        x += 2
+        delay(0.01)
+    while y < 560:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(780, y)
+        y += 2
+        delay(0.01)
+    while x > 10:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 560)
+        x -= 2
+        delay(0.01)
+    while y > 90:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(10, y)
+        y -= 2
+        delay(0.01)
+    while x < 400:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        x += 2
+        delay(0.01)
 
-while (y < 560):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(780, y)
-    y = y + 2
-    delay(0.01)
-
-while (x > 10):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 560)
-    x = x - 2
-    delay(0.01)
-
-while (y > 90):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(10, y)
-    y = y - 2
-    delay(0.01)
-
-while (x < 400):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
-
-while ()
+    angle = -math.pi / 2
+    while angle > -math.pi / 2 - 2 * math.pi:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        x = center_x + radius * math.cos(angle)
+        y = center_y + radius * math.sin(angle)
+        character.draw_now(int(x), int(y))
+        angle -= 0.02  # 시계방향
+        delay(0.01)
 
 close_canvas()
